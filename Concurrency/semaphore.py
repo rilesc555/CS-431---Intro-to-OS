060e0a7bc02d1022de133e221b834bd7a04a00f9
rilesc555 <rilesconnors555@gmail.com>
2024-10-02 18:14:58 -0500

    next

--- Concurrency/semaphore.py
+++ Concurrency/semaphore.py
@@ -5,7 +5,7 @@ class Semaphore:
     def __init__(self, initial):
         # Create a condition variable to associate with a lock
         # This is used to synchronize threads
-        self.lock = threading.Condition(threading.Lock)
+        self.lock = threading.Condition(threading.Lock())
 
         self.value = initial
 
@@ -20,5 +20,3 @@ class Semaphore:
         with self.lock:
             self.value += 1
             self.lock.notify()
-
-    

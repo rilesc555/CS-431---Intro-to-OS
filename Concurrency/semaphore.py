e878f76239c0eda1902f4cf9b3dad12da3c45595
rilesc555 <rilesconnors555@gmail.com>
2024-10-02 17:48:41 -0500

    add files

---
+++ Concurrency/semaphore.py
@@ -0,0 +1,24 @@
+import threading
+
+
+class Semaphore:
+    def __init__(self, initial):
+        # Create a condition variable to associate with a lock
+        # This is used to synchronize threads
+        self.lock = threading.Condition(threading.Lock)
+
+        self.value = initial
+
+    def P(self):
+        # Acquire the lock if available
+        with self.lock:
+            while self.value <= 0:
+                self.lock.wait()
+            self.value -= 1
+
+    def V(self):
+        with self.lock:
+            self.value += 1
+            self.lock.notify()
+
+    
